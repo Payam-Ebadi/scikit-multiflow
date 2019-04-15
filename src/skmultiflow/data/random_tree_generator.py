8c105e9c9a5c653aef494ec527599160e983541f
Payam-Ebadi <39265293+Payam-Ebadi@users.noreply.github.com>
2019-04-15 14:21:52 +0200

    sample_random_state
    in the "Examples" section:
    The "sample_random_state" written in "sample_random_seed" by mistake.

--- src/skmultiflow/data/random_tree_generator.py
+++ src/skmultiflow/data/random_tree_generator.py
@@ -53,7 +53,7 @@ class RandomTreeGenerator(Stream):
     >>> # Imports
     >>> from skmultiflow.data.random_tree_generator import RandomTreeGenerator
     >>> # Setting up the stream
-    >>> stream = RandomTreeGenerator(tree_random_state=8873, sample_random_seed=69, n_classes=2, n_cat_features=2,
+    >>> stream = RandomTreeGenerator(tree_random_state=8873, sample_random_state=69, n_classes=2, n_cat_features=2,
     ... n_num_features=5, n_categories_per_cat_feature=5, max_tree_depth=6, min_leaf_depth=3,
     ... fraction_leaves_per_level=0.15)
     >>> stream.prepare_for_use()
